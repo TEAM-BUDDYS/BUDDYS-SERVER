cf351f3802c617d007295971e40d31e1441b050b
haerxeong <haeryeong04@naver.com>
2026-06-22 16:29:06 +0900

    init

---
+++ src/main/java/org/sopt/buddys/BuddysApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.buddys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sopt/buddys/BuddysApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.buddys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
