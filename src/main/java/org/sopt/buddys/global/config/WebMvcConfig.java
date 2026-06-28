af75d2f2a75a28516723e133bedb87402d84e5c0
haerxeong <haeryeong04@naver.com>
2026-06-29 03:59:00 +0900

    feat: LoginUser 어노테이션 구현

--- src/main/java/org/sopt/buddys/global/config/WebMvcConfig.java
+++ src/main/java/org/sopt/buddys/global/config/WebMvcConfig.java
@@ -1,13 +1,24 @@
 package org.sopt.buddys.global.config;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.sopt.buddys.global.security.resolver.LoginUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
   private static final long MAX_AGE_SECS = 3600;
+  private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    resolvers.add(loginUserArgumentResolver);
+  }
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
