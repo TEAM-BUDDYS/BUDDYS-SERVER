4387f61111b2545883c1e4c1e6388bddf2ff703d
haerxeong <haeryeong04@naver.com>
2026-07-01 03:36:40 +0900

    fix: JPQL 쿼리 파라미터에 @Param 어노테이션 추가

--- src/main/java/org/sopt/buddys/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/org/sopt/buddys/domain/auth/repository/RefreshTokenRepository.java
@@ -5,6 +5,7 @@ import org.sopt.buddys.domain.auth.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
@@ -12,5 +13,5 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
   @Modifying
   @Query("DELETE FROM RefreshToken rt WHERE rt.userId = :userId")
-  void deleteByUserId(Long userId);
+  void deleteByUserId(@Param("userId") Long userId);
 }
