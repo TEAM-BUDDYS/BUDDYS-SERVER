6ac0b9ea78fcd078d5fb18ed04bda2d71cd9b86b
haerxeong <haeryeong04@naver.com>
2026-07-01 05:19:07 +0900

    fix: JwtAuthenticationFilter 및 application.yaml 설정 수정

--- src/main/java/org/sopt/buddys/global/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/org/sopt/buddys/global/security/jwt/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package org.sopt.buddys.global.security.jwt;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,6 +19,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import tools.jackson.databind.ObjectMapper;
 
 @Component
 @RequiredArgsConstructor
