e9027e1cb0a1913e1eef0cd91696629258c28ac2
Jyunee54 <jyunee54@gmail.com>
2026-07-02 21:57:49 +0900

    chore: 사용하지 않는 UserErrorCode 삭제

--- src/main/java/org/sopt/buddys/domain/user/code/UserErrorCode.java
+++ src/main/java/org/sopt/buddys/domain/user/code/UserErrorCode.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 
-  USER_NOT_FOUND("USER-E001", HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-  DELETED_USER("USER-E002", HttpStatus.FORBIDDEN, "탈퇴한 사용자입니다."),
-  UNIVERSITY_VERIFICATION_REQUIRED("USER-E003", HttpStatus.FORBIDDEN, "학교 인증이 필요합니다."),
-  EXCHANGE_VERIFICATION_REQUIRED("USER-E004", HttpStatus.FORBIDDEN, "파견교 인증이 필요합니다.");
+  USER_NOT_FOUND("USER-E001", HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
 
   private final String code;
   private final HttpStatus httpStatus;
