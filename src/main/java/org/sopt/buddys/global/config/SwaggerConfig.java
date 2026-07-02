30340b93a303137eab4f57e7f2759095184ad44f
Jyunee54 <jyunee54@gmail.com>
2026-07-02 15:38:52 +0900

    chore: 스웨거 설정 수정

--- src/main/java/org/sopt/buddys/global/config/SwaggerConfig.java
+++ src/main/java/org/sopt/buddys/global/config/SwaggerConfig.java
@@ -1,13 +1,18 @@
 package org.sopt.buddys.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+  private static final String JWT_SECURITY_SCHEME = "bearerAuth";
 
   @Bean
   public OpenAPI openAPI() {
@@ -18,6 +23,15 @@ public class SwaggerConfig {
 
     return new OpenAPI()
         .addServersItem(new Server().url("/"))
+        .components(new Components()
+            .addSecuritySchemes(
+                JWT_SECURITY_SCHEME,
+                new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")
+            ))
+        .addSecurityItem(new SecurityRequirement().addList(JWT_SECURITY_SCHEME))
         .info(info);
   }
 }
