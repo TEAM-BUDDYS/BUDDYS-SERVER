fad02d6307eee135e2af38aa87981c32c3a98330
Jyunee54 <jyunee54@gmail.com>
2026-07-02 17:52:33 +0900

    chore: Swagger 설정 변경

--- src/main/java/org/sopt/buddys/global/config/SwaggerConfig.java
+++ src/main/java/org/sopt/buddys/global/config/SwaggerConfig.java
@@ -1,7 +1,7 @@
 package org.sopt.buddys.global.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -12,26 +12,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-  private static final String JWT_SECURITY_SCHEME = "bearerAuth";
+  private static final String BEARER_AUTH = "bearerAuth";
 
   @Bean
   public OpenAPI openAPI() {
     Info info = new Info()
-        .title("BUDDYs API")
-        .version("1.0")
-        .description("38th Let's SOPT Buddys 서버 API 문서입니다.");
+            .title("BUDDYs API")
+            .version("1.0")
+            .description("38th Let's SOPT Buddys 서버 API 문서입니다.");
+
+    SecurityScheme bearerScheme = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT");
 
     return new OpenAPI()
-        .addServersItem(new Server().url("/"))
-        .components(new Components()
-            .addSecuritySchemes(
-                JWT_SECURITY_SCHEME,
-                new SecurityScheme()
-                    .type(SecurityScheme.Type.HTTP)
-                    .scheme("bearer")
-                    .bearerFormat("JWT")
-            ))
-        .addSecurityItem(new SecurityRequirement().addList(JWT_SECURITY_SCHEME))
-        .info(info);
+            .addServersItem(new Server().url("/"))
+            .info(info)
+            .components(new Components().addSecuritySchemes(BEARER_AUTH, bearerScheme))
+            .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH));
   }
 }
