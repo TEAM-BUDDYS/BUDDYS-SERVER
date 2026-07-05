8a53a0152d80d34d9177f2bf205455fc6cce9e01
Jyunee54 <jyunee54@gmail.com>
2026-07-05 20:43:26 +0900

    chore: Swagger 예시 수정

--- src/main/java/org/sopt/buddys/domain/chat/dto/request/CreateChatRoomRequest.java
+++ src/main/java/org/sopt/buddys/domain/chat/dto/request/CreateChatRoomRequest.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateChatRoomRequest(
-    @Schema(description = "채팅방을 생성할 상대방 사용자 ID", example = "20")
+    @Schema(description = "채팅방을 생성할 상대방 사용자 ID", example = "2")
     @NotNull
     Long participantUserId
 ) {
