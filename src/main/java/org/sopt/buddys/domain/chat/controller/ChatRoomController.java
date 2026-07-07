889a02abe917aebb73b92f0a7608da12a2b6cd59
Jyunee54 <jyunee54@gmail.com>
2026-07-07 15:49:00 +0900

    chore: import 제거

--- src/main/java/org/sopt/buddys/domain/chat/controller/ChatRoomController.java
+++ src/main/java/org/sopt/buddys/domain/chat/controller/ChatRoomController.java
@@ -18,7 +18,6 @@ import org.sopt.buddys.global.response.BaseResponse;
 import org.sopt.buddys.global.security.annotation.LoginUser;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
