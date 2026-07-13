2662a0b373f19a227e1f752cfe4485b2f006db3f
Jyunee54 <jyunee54@gmail.com>
2026-07-14 00:06:54 +0900

    feat: prometheus 경로 설정

--- src/main/java/org/sopt/buddys/global/config/SecurityConfig.java
+++ src/main/java/org/sopt/buddys/global/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
             .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
             .requestMatchers("/api/v1/auth/login", "/api/v1/auth/reissue", "/api/v1/auth/signup", "/api/v1/auth/kakao").permitAll()
             .requestMatchers("/api/v1/tags/**").permitAll()
-            .requestMatchers("/actuator/health").permitAll()
+            .requestMatchers("/actuator/health", "/actuator/prometheus").permitAll()
             .requestMatchers("/ws", "/ws/**").permitAll()
             .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
             .anyRequest().authenticated()
