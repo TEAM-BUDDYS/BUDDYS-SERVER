d2df4fbe049199df9b3d2e6c31536934d8d304a8
Kim Gayoon <kayoonkim@naver.com>
2026-07-14 20:08:02 +0900

    fix: 사용자 성별 선택값에서 OTHER 제거

--- src/main/java/org/sopt/buddys/domain/user/entity/Gender.java
+++ src/main/java/org/sopt/buddys/domain/user/entity/Gender.java
@@ -2,6 +2,5 @@ package org.sopt.buddys.domain.user.entity;
 
 public enum Gender {
   MALE,
-  FEMALE,
-  OTHER
+  FEMALE
 }
---
+++ src/main/resources/db/migration/V17__migrate_other_user_gender.sql
@@ -0,0 +1,3 @@
+UPDATE `user`
+SET gender = 'MALE'
+WHERE gender = 'OTHER';
