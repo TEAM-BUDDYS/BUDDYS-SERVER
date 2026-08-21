8d001e0874450a0a085a84ab926ad0c64a48c1e6
jyvnee <jyunee54@gmail.com>
2026-08-21 15:45:39 +0900

    feat: 탈퇴한 회원 정보 익명화

--- src/main/java/org/sopt/buddys/domain/user/controller/swagger/WithdrawUserSwagger.java
+++ src/main/java/org/sopt/buddys/domain/user/controller/swagger/WithdrawUserSwagger.java
@@ -13,7 +13,8 @@ import org.sopt.buddys.global.swagger.CommonErrorResponses;
 @Retention(RetentionPolicy.RUNTIME)
 @Operation(
     summary = "회원 탈퇴",
-    description = "로그인한 회원을 탈퇴 상태로 변경하고 리프레시 토큰을 폐기합니다. 작성한 콘텐츠는 유지됩니다."
+    description = "로그인한 회원을 탈퇴 상태로 변경하고 개인정보를 익명화합니다. "
+        + "리프레시 토큰과 개인화 데이터는 삭제하며, 작성한 콘텐츠는 유지됩니다."
 )
 @ApiResponses({
     @ApiResponse(responseCode = "200", description = "회원 탈퇴 성공"),
