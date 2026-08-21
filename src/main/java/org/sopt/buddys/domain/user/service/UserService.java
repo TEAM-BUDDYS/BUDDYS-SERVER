266547208509fca5b0006f7705252c4ba09eb410
jyvnee <jyunee54@gmail.com>
2026-08-21 15:45:29 +0900

    feat: 탈퇴한 회원의 북마크, 태그 정보 삭제

--- src/main/java/org/sopt/buddys/domain/user/service/UserService.java
+++ src/main/java/org/sopt/buddys/domain/user/service/UserService.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.sopt.buddys.domain.auth.repository.RefreshTokenRepository;
+import org.sopt.buddys.domain.place.repository.PlaceBookmarkRepository;
 import org.sopt.buddys.domain.post.entity.Post;
 import org.sopt.buddys.domain.post.repository.PostImageRepository;
 import org.sopt.buddys.domain.post.repository.PostRepository;
@@ -42,6 +43,7 @@ public class UserService {
   private final PostRepository postRepository;
   private final PostImageRepository postImageRepository;
   private final RefreshTokenRepository refreshTokenRepository;
+  private final PlaceBookmarkRepository placeBookmarkRepository;
 
   @Transactional
   public void withdraw(Long userId) {
@@ -49,6 +51,9 @@ public class UserService {
         .orElseThrow(() -> new BaseException(UserErrorCode.USER_NOT_FOUND));
 
     user.withdraw();
+    userRepository.saveAndFlush(user);
+    userTagRepository.deleteByUserId(userId);
+    placeBookmarkRepository.deleteByUserId(userId);
     refreshTokenRepository.deleteByUserId(userId);
   }
 
