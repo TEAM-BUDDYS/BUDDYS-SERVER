a7477aa163dd9c36140658d5e6ab47646674815a
jyvnee <jyunee54@gmail.com>
2026-08-29 21:38:39 +0900

    feat: 전체 태그 리스트 조회 API 구현

--- src/main/java/org/sopt/buddys/domain/tag/service/TagService.java
+++ src/main/java/org/sopt/buddys/domain/tag/service/TagService.java
@@ -1,7 +1,11 @@
 package org.sopt.buddys.domain.tag.service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
+import org.sopt.buddys.domain.tag.dto.response.TagGroupListResponse;
 import org.sopt.buddys.domain.tag.entity.Tag;
 import org.sopt.buddys.domain.tag.entity.TagType;
 import org.sopt.buddys.domain.tag.repository.TagRepository;
@@ -16,5 +20,13 @@ public class TagService {
 
   public List<Tag> getTagsByType(TagType type) {
     return tagRepository.findAllByTagTypeOrderByIdAsc(type);
+  }
+
+  public List<TagGroupListResponse> getAllTagGroups() {
+    Map<TagType, List<Tag>> tagsByType = tagRepository.findAllByOrderByTagTypeAscIdAsc().stream()
+        .collect(Collectors.groupingBy(Tag::getTagType));
+    return Stream.of(TagType.ACTIVITY, TagType.INTEREST, TagType.TRAVEL_STYLE)
+        .map(type -> TagGroupListResponse.of(type, tagsByType.getOrDefault(type, List.of())))
+        .toList();
   }
 }
