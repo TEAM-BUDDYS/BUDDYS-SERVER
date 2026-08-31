3786b358245d9363bdab488d70a000f0a67456a2
jyvnee <jyunee54@gmail.com>
2026-09-01 01:14:05 +0900

    feat: 코스 리스트 조회 controller 구현

---
+++ src/main/java/org/sopt/buddys/domain/user/controller/swagger/GetMyCoursesSwagger.java
@@ -0,0 +1,23 @@
+package org.sopt.buddys.domain.user.controller.swagger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.sopt.buddys.global.swagger.CommonErrorResponses;
+import org.sopt.buddys.global.swagger.InvalidRequestResponse;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Operation(summary = "내가 작성한 코스 목록 조회", description = "로그인한 사용자가 작성한 코스 목록을 조회합니다.")
+@ApiResponses({
+    @ApiResponse(responseCode = "200", description = "내가 작성한 코스 목록 조회 성공")
+})
+@InvalidRequestResponse
+@UserNotFoundResponse
+@CommonErrorResponses
+public @interface GetMyCoursesSwagger {
+}
