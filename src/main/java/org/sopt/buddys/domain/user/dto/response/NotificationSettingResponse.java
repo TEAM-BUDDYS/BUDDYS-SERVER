a00c5c6f3e872c041f23e03de6303bc7c33134cd
jyvnee <jyunee54@gmail.com>
2026-08-31 11:08:41 +0900

    chore: requiredMode = Schema.RequiredMode.REQUIRED 추가

--- src/main/java/org/sopt/buddys/domain/user/dto/response/NotificationSettingResponse.java
+++ src/main/java/org/sopt/buddys/domain/user/dto/response/NotificationSettingResponse.java
@@ -3,7 +3,11 @@ package org.sopt.buddys.domain.user.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record NotificationSettingResponse(
-    @Schema(description = "알림 설정 여부", example = "true")
+    @Schema(
+            description = "알림 설정 여부",
+            example = "true",
+            requiredMode = Schema.RequiredMode.REQUIRED
+    )
     boolean notificationEnabled
 ) {
 
