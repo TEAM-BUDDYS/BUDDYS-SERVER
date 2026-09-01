d151d843f8b5f5607eb97d79c8b44990dbd26662
haerxeong <haeryeong04@naver.com>
2026-09-01 18:57:27 +0900

    fix: 장소 저장의 동시 요청 경합에서 예외 처리가 무력화되는 문제 수정

--- src/main/java/org/sopt/buddys/domain/place/repository/PlaceBookmarkRepository.java
+++ src/main/java/org/sopt/buddys/domain/place/repository/PlaceBookmarkRepository.java
@@ -8,10 +8,22 @@ import org.sopt.buddys.domain.place.entity.PlaceBookmarkId;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface PlaceBookmarkRepository extends JpaRepository<PlaceBookmark, PlaceBookmarkId> {
+
+  @Modifying
+  @Query(value = """
+      INSERT INTO place_bookmark (user_id, place_id, created_at)
+      VALUES (:userId, :placeId, CURRENT_TIMESTAMP(6))
+      ON DUPLICATE KEY UPDATE created_at = created_at
+      """, nativeQuery = true)
+  int insertOrKeep(
+      @Param("userId") Long userId,
+      @Param("placeId") Long placeId
+  );
 
   @Query("""
       select pb.place.googlePlaceId
